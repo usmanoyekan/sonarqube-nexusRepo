dfd2f3fe69ab9a01b624d6dae6fd51c71b6cc7b7
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-12-07 21:05:30 -0600

    Update ec2.tf

--- tomcat/ec2.tf
+++ tomcat/ec2.tf
@@ -90,6 +90,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.tomcat_ec2_security_group.id]
   key_name               = "devopskeypair"
+   user_data = "${file("install_tomcat.sh")}"
 
   tags = {
     Name = "tomcat_server"
@@ -97,37 +98,7 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
-# an empty resource block
-resource "null_resource" "name" {
-
-  # ssh into the ec2 instance 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
-
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
-  provisioner "file" {
-    source      = "install_tomcat.sh"
-    destination = "/tmp/install_tomcat.sh"
-  }
-
-  # set permissions and run the install_tomcat.sh file
-  provisioner "remote-exec" {
-    inline = [
-        "sudo chmod +x /tmp/install_tomcat.sh",
-        "sh /tmp/install_tomcat.sh",
-    ]
-  }
-
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
-}
-
-
-# print the url of the tomcat server
+# print the url of the jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+  value     = join("", ["http://", aws_instance.ec2_instance.public_ip, ":", "8080"])
 }
