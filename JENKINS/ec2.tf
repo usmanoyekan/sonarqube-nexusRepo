dc12d89491642d9f8a7cc043b8f525168cd10801
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2022-12-07 20:46:05 -0600

    Update ec2.tf

--- JENKINS/ec2.tf
+++ JENKINS/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "yusuf"
+  profile   = "default"
 }
 
 
@@ -50,7 +50,24 @@ resource "aws_security_group" "ec2_security_group" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+  } 
+  ingress {
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  
+   ingress {
+    description      = "https access"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port        = 0
@@ -89,7 +106,8 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "devopskeypair"
+  key_name               = "bada"
+  user_data = "${file("install_jenkins.sh")}"
 
   tags = {
     Name = "jenkins_server"
@@ -97,37 +115,9 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
-# an empty resource block
-resource "null_resource" "name" {
-
-  # ssh into the ec2 instance 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
-
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
-  provisioner "file" {
-    source      = "install_jenkins.sh"
-    destination = "/tmp/install_jenkins.sh"
-  }
-
-  # set permissions and run the install_jenkins.sh file
-  provisioner "remote-exec" {
-    inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
-    ]
-  }
-
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
-}
 
 
 # print the url of the jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+  value     = join("", ["http://", aws_instance.ec2_instance.public_ip, ":", "8080"])
 }
