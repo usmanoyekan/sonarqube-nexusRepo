7bf769700bc608351fd5b7326d22a0ef9fa6128f
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2023-04-16 08:03:07 -0500

    Update install_jenkins.sh

--- JENKINS/install_jenkins.sh
+++ JENKINS/install_jenkins.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 62A9756BFD780C377CF24BA8FCEF32E745F2C3D5
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt install ca-certificates
